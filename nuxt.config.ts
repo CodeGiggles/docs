6840ae631f12877392596cb794ee25fcad5b0468
A-fauzi <akhmadfauzi0101@gmail.com>
2024-11-21 01:51:14 +0700

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,16 @@ export default defineNuxtConfig({
     '@nuxtjs/plausible'
   ],
 
-  compatibilityDate: '2024-10-22'
+  compatibilityDate: '2024-10-22',
+
+  head: {
+    meta: [
+      // Meta untuk Twitter Card
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },  // Jenis Twitter Card
+      { hid: 'twitter:title', name: 'twitter:title', content: 'CodeGiggles Docs' },  // Judul Twitter Card
+      { hid: 'twitter:description', name: 'twitter:description', content: 'Dokumentasi Materi Pemrograman' },  // Deskripsi Twitter Card
+      { hid: 'twitter:image', name: 'twitter:image', content: '/cover.png' },  // Gambar untuk Twitter Card
+      { hid: 'twitter:site', name: 'twitter:site', content: '@feelemptyz' },  // Handle Twitter situs lo
+    ]
+  }
 })
